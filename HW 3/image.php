fcca9841e7607d417ba5fb603de75f1f192d387a
HDmitriy <76693042+HDmitriy@users.noreply.github.com>
2022-05-03 14:26:24 +0300

    HW3

---
+++ HW 3/image.php
@@ -0,0 +1,19 @@
+<?php
+include 'Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+try {
+  $loader = new Twig_Loader_Filesystem('templates');
+  
+  $twig = new Twig_Environment($loader);
+  
+  $template = $twig->loadTemplate('bigimg.tmpl');
+
+  echo $template->render(array (
+  ));
+  
+} catch (Exception $e) {
+  die ('ERROR: ' . $e->getMessage());
+}
+?>
+<img src="<?='img/big/'.$_GET['photo'] ?>">
